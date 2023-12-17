65535dcf184bb15015c30deeb36767618dca464e
Ali Nacafov <alinacaf10@gmail.com>
2023-12-17 15:40:34 +0400

    write get method with jpa

---
+++ src/main/java/com/example/restfull/webservices/user/UserJpaResource.java
@@ -0,0 +1,65 @@
+package com.example.restfull.webservices.user;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+@RestController
+public class UserJpaResource {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserDaoService userService;
+    @GetMapping("/jpa/users")
+    public List<User> retrieveAllUsers(){
+
+        return userRepository.findAll();
+    };
+
+    @GetMapping("/jpa/users/{id}")
+    public EntityModel<Optional<User>> retireveUser(@PathVariable int id){
+
+       Optional<User>  user =  userRepository.findById(id);
+        if(!user.isPresent()){
+            throw new UserNotFoundException("id-"+id);
+
+        }
+
+        EntityModel<Optional<User>> model = EntityModel.of(user);
+
+        WebMvcLinkBuilder linkToUser = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        model.add(linkToUser.withRel("all-users"));
+
+        return model;
+    }
+    @DeleteMapping("/jpa/users/{id}")
+    public void deleteUser(@PathVariable int id){
+
+        User user =  userService.deleteById(id);
+        if(user==null){
+            throw new UserNotFoundException("id-"+id);
+
+        }
+
+    }
+
+    @PostMapping("/jpa/users")
+    public ResponseEntity<Object> createUser(@Valid  @RequestBody User user){
+        User savedUser = userService.saveUser(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/id").buildAndExpand(savedUser.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+}
