3f49c538dc510934053b3746aef713143c105b70
farhadnajafov <farhad.najafov@e-manat.az>
2023-12-18 08:06:54 +0400

    create post and delete methods with jpa

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO users (id, birth_date, name) VALUES ('1', CURRENT_TIMESTAMP,'Dean' );
-INSERT INTO users (id, birth_date, name) VALUES ('2', CURRENT_TIMESTAMP, 'Deanwdwd');
+INSERT INTO users (id, birth_date, name) VALUES ('1001', CURRENT_TIMESTAMP,'Dean' );
+INSERT INTO users (id, birth_date, name) VALUES ('1002', CURRENT_TIMESTAMP, 'Deanwdwd');
